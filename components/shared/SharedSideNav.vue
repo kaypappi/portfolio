d148356690380aca3aa82e208c2b4ad23f8a87ea
kaypappi <wesleyukadike@yahoo.com>
2023-02-18 07:38:01 +0100

    fix menu height on mobile

--- components/shared/SharedSideNav.vue
+++ components/shared/SharedSideNav.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav :style="{ height:`${height}px`,height:'100dvh'}" class=" fixed top-0  left-0 right-0 bottom-0 z-50 w-full bg-slate-900">
+    <nav class=" fixed top-0 h-screen left-0 right-0 bottom-0 z-50 w-full bg-slate-900">
         <Icon @click="toggleSideNav" name="uil:times" size="32" class=" cursor-pointer absolute right-4 top-4" />
         <ul class=" h-full  flex flex-col w-full gap-12  justify-center items-center  text-slate-500 font-aeonik-bold">
             <NuxtLink @click.native="toggleSideNav" to="/#about">ABOUT</NuxtLink>
