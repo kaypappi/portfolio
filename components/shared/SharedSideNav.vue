590c21ba45e1e82016b02c3c682271884ed8cb2b
kaypappi <wesleyukadike@yahoo.com>
2023-02-18 05:26:23 +0100

    clean up nav

--- components/shared/SharedSideNav.vue
+++ components/shared/SharedSideNav.vue
@@ -1,15 +1,26 @@
 <template>
-    <nav :style="{height:'100svh'}"  class=" fixed top-0  left-0 right-0 bottom-0 z-50 w-full bg-slate-900">
+    <nav :style="{ height: '100svh' }" class=" fixed top-0  left-0 right-0 bottom-0 z-50 w-full bg-slate-900">
         <Icon @click="toggleSideNav" name="uil:times" size="32" class=" cursor-pointer absolute right-4 top-4" />
-        <ul  class=" h-full flex flex-col w-full  justify-center items-center  text-slate-500 font-aeonik-bold">
+        <ul class=" h-full flex flex-col w-full gap-12  justify-center items-center  text-slate-500 font-aeonik-bold">
             <NuxtLink @click.native="toggleSideNav" to="/#about">ABOUT</NuxtLink>
-            <NuxtLink @click.native="toggleSideNav" class=" py-12" to="/#works">WORKS</NuxtLink>
+            <NuxtLink @click.native="toggleSideNav" class="" to="/#works">WORKS</NuxtLink>
             <NuxtLink @click.native="toggleSideNav" to="/#experience">EXPERIENCE</NuxtLink>
+            <NuxtLink :to="`${footer.cv}?dl=`" target="_blank">
+                <button
+                    class=" max-w-max px-8 py-3 rounded-full bg-white ring ring-white  text-gray-800 hover:bg-transparent hover:text-white">Download
+                    CV</button>
+            </NuxtLink>
         </ul>
-    </nav>
+</nav>
 </template>
 <script setup>
-const props= defineProps({
-    toggleSideNav:Function
+const props = defineProps({
+    toggleSideNav: Function
 })
+const sanity = useSanity()
+const query = groq`*[_type == "footer"][0]{
+    ...,
+    'cv':cv.asset->url
+}`
+const { data: footer, refresh } = await useAsyncData('footer', () => sanity.fetch(query))
 </script>
